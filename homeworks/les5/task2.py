f692164c51aa5502a1c2e7eaf1e0306a49bbdb9f
leonardar <leonardar88@gmail.com>
2020-06-29 14:18:56 +0300

    fixed task2

--- homeworks/les5/task2.py
+++ homeworks/les5/task2.py
@@ -2,7 +2,7 @@
 # количества слов в каждой строке.
 
 if __name__ == '__main__':
-    with open("file2.txt") as in_file:
+    with open('file2.txt') as in_file:
         line_counter = 0
         word_counters = []
         for line in in_file:
@@ -12,6 +12,12 @@ if __name__ == '__main__':
                 word_counter += 1
             word_counters.append(word_counter)
 
-    print(f'Всего: {line_counter} строк')
+    print(f'Всего строк: {line_counter}')
     for i, word_counter in enumerate(word_counters):
-        print(f'{i+1} строка содержит {word_counter} сл.')
+        figure = word_counter % 10
+        if figure == 1 and word_counter != 11:
+            print(f'{i+1}-я строка содержит {word_counter} слово')
+        elif 1 < figure < 5 and not 11 < word_counter < 15:
+            print(f'{i+1}-я строка содержит {word_counter} слова')
+        else:
+            print(f'{i+1}-я строка содержит {word_counter} слов')
